3b1d33cb7d7f646de8373b562e65ca423b9c9d54
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-17 04:58:30 +0000

    av

--- shell_exe/prompt.c
+++ shell_exe/prompt.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-prototype
+ *Return: 0
+ */
 
+int main (void)
+{
+	char *buffer = malloc(1024);
+	size_t len = 1024;
+
+	while (1)
+	{
+		printf("$ ");
+		getline(&buffer, &len, stdin);
+		printf("%s", buffer);
+
+	}
+	return (0);
+}
