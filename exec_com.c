87268855b33d2bf0f710e5cbfdb9861ebe6bcaeb
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-27 07:52:19 +0000

    function execute_command revise to getenv

--- exec_com.c
+++ exec_com.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,105 +9,55 @@
  *
 */
 
-extern char **environ; /* Declare the global environment variable */
-
-char *find_path(char *command)
-{
-    char *path = NULL, *dir, *full_path;
-    struct stat st;
-    int i = 0;
-
-    /* Search for PATH in the environ array */
-    while (environ[i])
-    {
-        if (strncmp(environ[i], "PATH=", 5) == 0)
-        {
-            path = environ[i] + 5; /* Skip "PATH=" */
-            break;
-        }
-        i++;
-    }
-
-    if (!path || strcmp(path, "") == 0)
-    {
-        /* Fallback to /bin if PATH is empty */
-        path = "/bin";
-    }
-
-    dir = strtok(path, ":");
-    while (dir)
-    {
+char *get_path(char *command) {
+    char *path = getenv("PATH");
+    char *path_copy = strdup(path);
+    char *dir = strtok(path_copy, ":");
+    char *full_path = NULL;
+    
+    while (dir) {
         full_path = malloc(strlen(dir) + strlen(command) + 2);
-        if (!full_path)
-        {
-            perror("Error allocating memory");
-            return NULL;
-        }
-
         sprintf(full_path, "%s/%s", dir, command);
-
-        if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
-        {
-            return full_path; /* Command found */
+        
+        if (access(full_path, X_OK) == 0) {
+            free(path_copy);
+            return full_path;
         }
-
+        
         free(full_path);
         dir = strtok(NULL, ":");
     }
-
-    return NULL; /* Command not found */
+    
+    free(path_copy);
+    return NULL;
 }
-/**
- *
- */
-void execute_command(char **args)
-{
-    char *path;
-    pid_t pid;
-    int status;
 
-    if (args[0] == NULL)
-        return; /* Ignore empty input */
-
-    /* Trim leading and trailing spaces here if needed */
-
-    if (args[0][0] == '/' || args[0][0] == '.')
-        path = args[0];
-    else
-        path = find_path(args[0]);
-
-    if (!path)
-    {
-        write(STDERR_FILENO, args[0], strlen(args[0]));
-        write(STDERR_FILENO, ": Command not found\n", 20);
-        return;
-    }
-
-    pid = fork();
-    if (pid == -1)
-    {
-        perror(args[0]);
-        if (path != args[0])
-            free(path);
-        return;
-    }
-
-    if (pid == 0)
-    {
-        /* Execute the command */
-        if (execve(path, args, environ) == -1)
-        {
-            perror(args[0]);
-            exit(EXIT_FAILURE);
+void execute_command(char **args) {
+    char *command = args[0];
+    char *full_path = NULL;
+	pid_t pid = fork();
+    if (strchr(command, '/')) {
+        if (access(command, X_OK) == 0) {
+            full_path = strdup(command);
         }
+    } else {
+        full_path = get_path(command);
     }
-    else
-    {
-        /* Parent process waits */
-        wait(&status);
+    
+    if (!full_path) {
+        fprintf(stderr, "./shell: No such file or directory\n");
+        return;
     }
-
-    if (path != args[0])
-        free(path);
+    
+    if (pid == 0) {
+        execve(full_path, args, environ);
+        perror("execve");
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        wait(NULL);
+    } else {
+        perror("fork");
+    }
+    
+    free(full_path);
 }
-
