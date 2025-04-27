4a7f2256b2aa64a91faebe9179e8073bc8e2428c
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-27 11:53:54 +0000

    handle empty path

--- exec_com.c
+++ exec_com.c
@@ -9,121 +9,137 @@
 
 extern char **environ;
 
+/**
+ * get_path_env - Gets the PATH environment variable
+ * Return: PATH value or NULL if not found
+ */
 char *get_path_env(void)
 {
-    int i;
-
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        if (strncmp(environ[i], "PATH=", 5) == 0)
-        {
-            /* Skip "PATH=" and return the value */
-            return (environ[i] + 5);
-        }
-    }
-    return (NULL); /* PATH not found */
+    char *path = getenv("PATH");
+    return path ? path : NULL;
 }
 
-char *find_path(char *command)
+/**
+ * find_command_path - Finds the full path of a command
+ * @command: The command to find
+ * Return: Full path if found, NULL otherwise
+ */
+char *find_command_path(char *command)
 {
     char *path_env, *path_copy, *dir;
-    char *full_path;
-    size_t full_len;
+    char *full_path = NULL;
+    struct stat st;
 
     if (!command)
         return NULL;
 
-    /* Get PATH from the environment */
-    path_env = get_path_env();
-    if (!path_env)
+    /* Check if command is already a full path */
+    if (command[0] == '/' || command[0] == '.') {
+        if (stat(command, &st) == 0 && (st.st_mode & S_IXUSR))
+            return strdup(command);
         return NULL;
+    }
 
-    /* Make a copy because strtok modifies the string */
-    path_copy = strdup(path_env);
-    if (!path_copy)
+    /* Get PATH environment variable */
+    path_env = get_path_env();
+
+    /* Handle empty PATH case - check current directory only */
+    if (path_env == NULL || *path_env == '\0') {
+        char *cwd = getcwd(NULL, 0);
+        if (!cwd) return NULL;
+        
+        full_path = malloc(strlen(cwd) + strlen(command) + 2);
+        if (!full_path) {
+            free(cwd);
+            return NULL;
+        }
+        sprintf(full_path, "%s/%s", cwd, command);
+        free(cwd);
+
+        if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
+            return full_path;
+
+        free(full_path);
         return NULL;
+    }
+
+    /* Normal PATH lookup */
+    path_copy = strdup(path_env);
+    if (!path_copy) return NULL;
 
     dir = strtok(path_copy, ":");
-    while (dir != NULL)
-    {
-        /* Build full path string: dir + "/" + command + null */
-        full_len = strlen(dir) + 1 + strlen(command) + 1;
-        full_path = malloc(full_len);
-        if (!full_path)
-        {
+    while (dir != NULL) {
+        full_path = malloc(strlen(dir) + strlen(command) + 2);
+        if (!full_path) {
             free(path_copy);
             return NULL;
         }
+        sprintf(full_path, "%s/%s", dir, command);
 
-        strcpy(full_path, dir);
-        strcat(full_path, "/");
-        strcat(full_path, command);
-
-        /* Check if this file exists and is executable */
-        if (access(full_path, X_OK) == 0)
-        {
+        if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR)) {
             free(path_copy);
-            return full_path; /* SUCCESS: Return this path */
+            return full_path;
         }
 
-        /* Otherwise try next directory */
         free(full_path);
         dir = strtok(NULL, ":");
     }
 
-    /* Not found */
     free(path_copy);
     return NULL;
 }
+
 /**
  * execute_command - Executes a command with arguments
  * @args: NULL terminated array of arguments
  */
 void execute_command(char **args)
 {
-	char *path = NULL;
-	pid_t pid;
-	int status;
+    char *full_path = NULL;
+    pid_t pid;
+    int status;
 
-	if (!args || !args[0])
-		return;
+    if (!args || !args[0])
+        return;
 
-	if (strchr(args[0], '/'))
-		path = args[0];
-	else
-	{
-		path = find_path(args[0]);
-		if (!path)
-		{
-			write(STDERR_FILENO, "./hsh: 1: ", 10);
-			write(STDERR_FILENO, args[0], strlen(args[0]));
-			write(STDERR_FILENO, ": not found\n", 12);
-			exit(127); /* Exit with 127 when command not found */
-		}
-	}
+    /* Handle builtins first */
+    if (strcmp(args[0], "exit") == 0) {
+        shell_exit(args);
+        return;
+    }
+    else if (strcmp(args[0], "env") == 0) {
+        print_environment();
+        return;
+    }
+    else if (strcmp(args[0], "cd") == 0) {
+        change_directory(args);
+        return;
+    }
 
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		if (path != args[0])
-			free(path);
-		return;
-	}
+    /* Find command path */
+    full_path = find_command_path(args[0]);
+    if (!full_path) {
+        fprintf(stderr, "%s: command not found\n", args[0]);
+        exit(127); /* Exit with status 127 when command not found */
+    }
 
-	if (pid == 0)
-	{
-		if (execve(path, args, environ) == -1)
-		{
-			perror(args[0]);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		wait(&status);
-	}
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        free(full_path);
+        return;
+    }
 
-	if (path != args[0])
-		free(path);
+    if (pid == 0) {
+        /* Child process */
+        if (execve(full_path, args, environ) == -1) {
+            perror(args[0]);
+            free(full_path);
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        /* Parent process */
+        waitpid(pid, &status, 0);
+        free(full_path);
+    }
 }
