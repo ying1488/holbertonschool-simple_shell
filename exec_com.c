efbc7e9f4e5eba19abc6e2c30380f25341fa4335
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-28 08:20:24 +0000

    betty fixed

--- exec_com.c
+++ exec_com.c
@@ -9,177 +9,182 @@
 
 extern char **environ;
 
+/**
+ * get_path_env - Retrieve the value of the
+ * PATH environment variable
+ * Return: value of the PATH environment variable
+ * (without the "PATH=" prefix),
+ * or NULL if the "PATH" entry is not found.
+ */
 char *get_path_env(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        if (strncmp(environ[i], "PATH=", 5) == 0)
-        {
-            /* Skip "PATH=" and return the value */
-            return (environ[i] + 5);
-        }
-    }
-    return (NULL); /* PATH not found */
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+		{
+			/* Skip "PATH=" and return the value */
+			return (environ[i] + 5);
+		}
+	}
+	return (NULL); /* PATH not found */
 }
+
+/**
+ * find_path - Search for a command in directories listed
+ * in the PATH environment variable
+ * @command: The command name to search for.
+ * Return: The full path of the executable,
+ * NULL if the command is not found or is not executable.
+ */
 
 char *find_path(char *command)
 {
-    char *path_env, *path_copy, *dir;
-    char *full_path;
-    size_t full_len;
+	char *path_env, *path_copy, *dir;
+	char *full_path;
+	size_t full_len;
 
-    if (!command)
-        return NULL;
+	if (!command)
+		return (NULL);
+	/* Get PATH from the environment */
+	path_env = get_path_env();
+	if (!path_env)
+		return (NULL);
+	/* Make a copy because strtok modifies the string */
+	path_copy = strdup(path_env);
+	if (!path_copy)
+		return (NULL);
 
-    /* Get PATH from the environment */
-    path_env = get_path_env();
-    if (!path_env)
-        return NULL;
+	dir = strtok(path_copy, ":");
+	while (dir != NULL)
+	{
+		/* Build full path string: dir + "/" + command + null */
+		full_len = strlen(dir) + 1 + strlen(command) + 1;
+		full_path = malloc(full_len);
+		if (!full_path)
+			free(path_copy);
+			return (NULL);
+		strcpy(full_path, dir);
+		strcat(full_path, "/");
+		strcat(full_path, command);
 
-    /* Make a copy because strtok modifies the string */
-    path_copy = strdup(path_env);
-    if (!path_copy)
-        return NULL;
+		/* Check if this file exists and is executable */
+		if (access(full_path, X_OK) == 0)
+			free(path_copy);
+			return (full_path); /* SUCCESS: Return this path */
+		/* Otherwise try next directory */
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+	/* Not found */
+	free(path_copy);
+	return (NULL);
+}
 
-    dir = strtok(path_copy, ":");
-    while (dir != NULL)
-    {
-        /* Build full path string: dir + "/" + command + null */
-        full_len = strlen(dir) + 1 + strlen(command) + 1;
-        full_path = malloc(full_len);
-        if (!full_path)
-        {
-            free(path_copy);
-            return NULL;
-        }
 
-        strcpy(full_path, dir);
-        strcat(full_path, "/");
-        strcat(full_path, command);
 
-        /* Check if this file exists and is executable */
-        if (access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return full_path; /* SUCCESS: Return this path */
-        }
+/**
+ * handle_builtin_exit - Handle the 'exit' built-in command
+ * @args: Null-terminated array of arguments passed to the shell
+* Return: 1 if 'exit' command is executed, 0 otherwise
+ */
+int handle_builtin_exit(char **args)
+{
+	if (_strcmp(args[0], "exit") == 0)
+	{
+		if (isatty(STDIN_FILENO))  /* Check if shell is interactive */
+		{
+			free_args(args);
+			exit(EXIT_SUCCESS);
+		}
+		return (1); /* Exit command is handled */
+	}
+	return (0);  /* Command is not exit */
+}
 
-        /* Otherwise try next directory */
-        free(full_path);
-        dir = strtok(NULL, ":");
-    }
+/**
+ * get_command_path - Find the full path of a command
+ * @args: Null-terminated array of arguments passed to the shell
+ * Return: A string containing the full path to the command if found,
+ */
+char *get_command_path(char **args)
+{
+	char *path = NULL;
 
-    /* Not found */
-    free(path_copy);
-    return NULL;
+	if (_strchr(args[0], '/'))
+	{
+		path = _strdup(args[0]);
+	}
+	else
+	{
+		path = find_path(args[0]);
+	}
+	return (path);
 }
 /**
- * execute_command - Execute a command
- * @args: Null-terminated array of arguments
+ * execute_fork - Fork the process and execute a command
+ * @path: Full path to the command to be executed
+ * @args: Null-terminated array of arguments passed to the shell
+ * Return: The status code of the executed command.
  */
-#include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <sys/wait.h>
+int execute_fork(char *path, char **args)
+{
+	pid_t pid;
+	int status;
 
-extern char **environ;
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (-1);  /* Fork failed */
+	}
+
+	if (pid == 0)
+	{
+		if (execve(path, args, environ) == -1)
+		{
+			perror(args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
+	return (status);
+}
 
 /**
- * execute_command - Execute a command
- * @args: Null-terminated array of arguments
- */
+ * execute_command - Execute a command in a new process
+ * @args: Null-terminated array of arguments passed to the shell
+  */
 void execute_command(char **args)
 {
-    static int last_status = 0;  /* Store the last exit status */
-    pid_t pid;
-    int status;
-    char *path = NULL;
+	static int last_status;  /* Store the last exit status */
+	char *path = NULL;
 
-    if (!args || !args[0])
-        return;
-
-    /* Handle built-in command: exit */
-    if (_strcmp(args[0], "exit") == 0)
-    {
-        free_args(args);
-        exit(last_status);  /* Exit the shell with the last command's status */
-    }
- /* Handle built-in command: env */
-
-    if (_strcmp(args[0], "env") == 0)
-
-    {
-
-        int i = 0;
-
-        while (environ[i] != NULL)
-
-        {
-
-            printf("%s\n", environ[i]);  /* Print each environment variable */
-
-            i++;
-
-        }
-
-        return;  /* No need to fork or execute anything else */
-
-    }
-    if (_strchr(args[0], '/'))
-        path = _strdup(args[0]);
-    else
-        path = find_path(args[0]);
-
-    if (!path)  /* Command not found */
-    {
-        write(STDERR_FILENO, "./hsh: 1: ", 10);
-        write(STDERR_FILENO, args[0], _strlen(args[0]));
-        write(STDERR_FILENO, ": not found\n", 12);
-
-        last_status = 127;  /* Set last_status to indicate command not found */
-
-        if (!isatty(STDIN_FILENO))
-        {
-            free_args(args);
-            exit(last_status);  /* Exit with the "not found" error status */
-        }
-        return;
-    }
-
-    pid = fork();
-    if (pid == -1)  /* Error with forking */
-    {
-        perror("fork");
-        free(path);
-        return;
-    }
-
-    if (pid == 0)  /* Child process */
-    {
-        if (execve(path, args, environ) == -1)  /* Execute the command */
-        {
-            perror(args[0]);  /* Print error if execve fails */
-            free(path);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else  /* Parent process */
-    {
-        waitpid(pid, &status, 0);  /* Wait for child to finish */
-        if (WIFEXITED(status)) /* Check if the child process exited normally*/
-            last_status = WEXITSTATUS(status);  /* capture normal exit status */
-        else if (WIFSIGNALED(status)) /**If the process was terminated by a signal*/
-            last_status = 128 + WTERMSIG(status);  /* Set exit status based on signal*/
-	else
-            last_status = 0;  /* Default case for abnormal exit */
-    }
-
-    free(path);  /* Free the path allocated for the command */
+	if (!args || !args[0])
+		return;
+	/* Handle built-in command: exit */
+	if (handle_builtin_exit(args)
+		return;
+	/* Find command path */
+	path = get_command_path(args);
+	if (!path)
+	{
+		write(STDERR_FILENO, "./hsh: 1: ", 10);
+		write(STDERR_FILENO, args[0], _strlen(args[0]));
+		write(STDERR_FILENO, ": not found\n", 12);
+		last_status = 127;  /* Set last_status to indicate command not found */
+		if (!isatty(STDIN_FILENO))
+		{
+			free_args(args);
+			exit(last_status);  /* Exit with the appropriate status */
+		}
+		return;
+		}
+	    /* Fork and execute the command */
+	    last_status = execute_fork(path, args);
+	    free(path);
 }
-
