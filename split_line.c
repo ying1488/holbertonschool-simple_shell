4fbdd22375648e8672f2ae0d1d9361b3ba9c8a87
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-28 11:59:37 +0000

    betty fixed for split_line.c

--- split_line.c
+++ split_line.c
@@ -1,34 +1,40 @@
 #include "shell.h"
 
+/**
+ * split_line - Splits a line of input into tokens (arguments)
+ * @line: The input line to be split
+ *
+ * Return: A null-terminated array of strings (tokens)
+ */
+
 char **split_line(char *line)
 {
-    int bufsize = 64;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    char *token;
-    int position = 0;
+	int bufsize = 64;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+	int position = 0;
 
-    if (!tokens)
-    {
-        perror("Error allocating memory");
-        exit(EXIT_FAILURE);
-    }
-
-    token = strtok(line, " \t\n");
-    while (token != NULL)
-    {
-        tokens[position++] = token;
-        if (position >= bufsize)
-        {
-            bufsize += 64;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens)
-            {
-                perror("Error reallocating memory");
-                exit(EXIT_FAILURE);
-            }
-        }
-        token = strtok(NULL, " \t\n");
-    }
-    tokens[position] = NULL;
-    return tokens;
+	if (!tokens)
+	{
+		perror("Error allocating memory");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(line, " \t\n");
+	while (token != NULL)
+	{
+		tokens[position++] = token;
+		if (position >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+				if (!tokens)
+				{
+					perror("Error reallocating memory");
+					exit(EXIT_FAILURE);
+				}
+		}
+		token = strtok(NULL, " \t\n");
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
