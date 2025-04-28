8f9daf46942c74542f9c01e8c8a0ee4e5e709d4c
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-28 12:55:46 +0000

    fixed betty code for shell.h

--- shell.h
+++ shell.h
@@ -12,9 +12,18 @@
 /* Global environment */
 extern char **environ;
 
-typedef struct {
-    int exit_status;
-    char *input_line;
+/**
+ * struct shell_ctx -  Represents the context/state
+ * of the shell session.
+ * @exit_status: The exit status code of the last executed command or
+ * the exit code the shell should return upon exiting.
+ * @input_line: A dynamically allocated string
+ * containing the most recent line of user input.
+ */
+typedef struct shell_ctx
+{
+	int exit_status;
+	char *input_line;
 } shell_ctx_t;
 
 /* Function prototypes */
@@ -23,7 +32,7 @@ char *get_command_path(char **args);
 char *find_path(char *command);
 char **split_line(char *line);
 void free_args(char **args);
-void prompt(void); /* if you have a function to print your shell prompt */
+void prompt(void);
 int shell_exit(char **args);
 void check_exit_builtin(char *input, shell_ctx_t *ctx);
 char *_strdup(const char *s);
