9d5f5a941339a0370e8ab072dba86f1aaf8e7475
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-28 11:27:26 +0000

    betty fixed for main.c

--- main.c
+++ main.c
@@ -6,31 +6,33 @@
 #include <sys/wait.h>
 #include "shell.h"
 
+/**
+ * main - Entry point for the simple shell program
+ * Return: Always returns 0 on successful execution.
+ */
+
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    char **args;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	char **args;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "($) ", 4);
-        
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            free(line);
-            exit(EXIT_SUCCESS);
-        }
-
-        args = split_line(line);
-        if (args[0] != NULL)
-            execute_command(args);
-
-        free(args);
-    }
-    free(line);
-    return 0;
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "($) ", 4);
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			free(line);
+			exit(EXIT_SUCCESS);
+		}
+		args = split_line(line);
+		if (args[0] != NULL)
+			execute_command(args);
+		free(args);
+	}
+	free(line);
+	return (0);
 }
