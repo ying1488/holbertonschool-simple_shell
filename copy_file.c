5bb75b10c5c8f7dd6d350c3fac16943a4216a138
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-28 11:20:52 +0000

    fixed betty for copy_file.c

--- copy_file.c
+++ copy_file.c
@@ -3,37 +3,42 @@
 #include <stdio.h>
 #include "shell.h"
 
+/**
+ * copy_file - Copies the contents of one file to another.
+ * @src: Path to the source file to be copied.
+ * @dest: Path to the destination file where content
+ * Return: void
+ */
+
 void copy_file(const char *src, const char *dest)
 {
-    int src_fd, dest_fd, nread;
-    char buffer[1024];
+	int src_fd, dest_fd, nread;
+	char buffer[1024];
 
-    src_fd = open(src, O_RDONLY);
-    if (src_fd < 0)
-    {
-        perror("Source file open failed");
-        return;
-    }
+	src_fd = open(src, O_RDONLY);
+	if (src_fd < 0)
+	{
+		perror("Source file open failed");
+		return;
+	}
+	dest_fd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0755);
+	if (dest_fd < 0)
+	{
+		perror("Destination file open failed");
+		close(src_fd);
+		return;
+	}
 
-    dest_fd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0755);
-    if (dest_fd < 0)
-    {
-        perror("Destination file open failed");
-        close(src_fd);
-        return;
-    }
-
-    while ((nread = read(src_fd, buffer, sizeof(buffer))) > 0)
-    {
-        if (write(dest_fd, buffer, nread) < 0)
-        {
-            perror("Write to destination failed");
-            close(src_fd);
-            close(dest_fd);
-            return;
-        }
-    }
-
-    close(src_fd);
-    close(dest_fd);
+	while ((nread = read(src_fd, buffer, sizeof(buffer))) > 0)
+	{
+		if (write(dest_fd, buffer, nread) < 0)
+		{
+			perror("Write to destination failed");
+			close(src_fd);
+			close(dest_fd);
+			return;
+		}
+	}
+	close(src_fd);
+	close(dest_fd);
 }
