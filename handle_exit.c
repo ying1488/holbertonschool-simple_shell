485d70c9338ddc1668bcfc76667a240effa83768
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-28 12:44:24 +0000

    betty fixed exit

--- handle_exit.c
+++ handle_exit.c
@@ -7,20 +7,18 @@
  */
 int _atoi(const char *s)
 {
-    int i = 0, result = 0;
+	int i = 0, result = 0;
 
-    if (!s)
-        return -1;
-
-    while (s[i])
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return -1; /* Not a valid number */
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
-
-    return result;
+	if (!s)
+		return (-1);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (-1); /* Not a valid number */
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return (result);
 }
 
 /**
@@ -29,20 +27,19 @@ int _atoi(const char *s)
  */
 void handle_exit(char **args)
 {
-    int exit_status = 0;
+	int exit_status = 0;
 
-    if (args[1]) /* If there is an argument */
-    {
-        exit_status = _atoi(args[1]);
-        if (exit_status == -1)
-        {
-            write(STDERR_FILENO, "./hsh: 1: exit: Illegal number: ", 31);
-            write(STDERR_FILENO, args[1], _strlen(args[1]));
-            write(STDERR_FILENO, "\n", 1);
-            return;
-        }
-    }
-
-    free_args(args);
-    exit(exit_status);
+	if (args[1]) /* If there is an argument */
+	{
+		exit_status = _atoi(args[1]);
+		if (exit_status == -1)
+		{
+			write(STDERR_FILENO, "./hsh: 1: exit: Illegal number: ", 31);
+			write(STDERR_FILENO, args[1], _strlen(args[1]));
+			write(STDERR_FILENO, "\n", 1);
+			return;
+		}
+	}
+	free_args(args);
+	exit(exit_status);
 }
